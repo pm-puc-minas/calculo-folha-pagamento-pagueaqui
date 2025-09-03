a37ab4243ad9cd7bd0fb6013b8eeca5114ea9a58
Milena Cardoso <mile.araujocar@gmail.com>
2025-09-02 21:56:41 -0300

    feat: add listAll e list

--- hrpayroll/src/main/java/com/example/hrpayroll/Service/UserService.java
+++ hrpayroll/src/main/java/com/example/hrpayroll/Service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.hrpayroll.Service;
 
+import com.example.hrpayroll.Model.UserModel;
 import com.example.hrpayroll.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,5 +16,18 @@ public class UserService {
         public UserService(UserRepository userRepository) {
                 this.userRepository = userRepository;
         }
+
+        // Listar todos
+        public List<UserModel> list() {
+                return userRepository.findAll();
+        }
+        // Listar id
+        public Optional<UserModel> findOne (Long id) {
+            Optional<UserModel> one;
+            one = userRepository.findOne(id);
+            return one;
+
+        }
+
 
 }
