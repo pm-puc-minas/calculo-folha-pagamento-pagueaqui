a455c556c77096c13886dcd2f8d202ee0f4619fe
caiofelixreis <caiofelix.reis@gmail.com>
2025-09-04 19:39:39 -0300

    fix: model improvement

--- hrpayroll/src/main/java/com/example/hrpayroll/Model/UserModel.java
+++ hrpayroll/src/main/java/com/example/hrpayroll/Model/UserModel.java
@@ -1,25 +1,27 @@
 package com.example.hrpayroll.Model;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class UserModel {
-@Id
-@Getter
-@Setter
-
-    private String nome;
-    private String email;
-    private String senha;
+    @Id
     private Long id;
 
-    //...
+    @NotBlank
+    private String nome;
 
+    @Email
+    private String email;
+
+    @NotBlank()
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Senha fraca. A senha deve ter no mínimo 8 caracteres, incluindo uma letra maiúscula, uma minúscula, um número e um caractere especial.")
+    private String senha;
 }
