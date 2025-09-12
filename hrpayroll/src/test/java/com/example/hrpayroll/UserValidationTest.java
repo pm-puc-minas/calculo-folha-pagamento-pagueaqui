b14213ffda4fdc8d77fee4a88eb37f07819a63cf
Milena Cardoso <mile.araujocar@gmail.com>
2025-09-12 18:00:36 -0300

    Inicialização do Validator antes dos testes

---
+++ hrpayroll/src/test/java/com/example/hrpayroll/UserValidationTest.java
@@ -0,0 +1,27 @@
+package com.example.hrpayroll;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+import java.util.Set;
+import com.example.hrpayroll.Model.UserModel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserValidationTest {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUpValidator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+
+}
