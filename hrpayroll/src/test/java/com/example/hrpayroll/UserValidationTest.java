dde5ed3d1df015b4ef41b4cf32e1700c15b7d23b
Milena Cardoso <mile.araujocar@gmail.com>
2025-09-12 18:01:57 -0300

    feat: implementando o primeiro teste: usuario em branco

--- hrpayroll/src/test/java/com/example/hrpayroll/UserValidationTest.java
+++ hrpayroll/src/test/java/com/example/hrpayroll/UserValidationTest.java
@@ -22,6 +22,18 @@ class UserValidationTest {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
     }
+    @Test
 
+    void whenNameBlank_thenViolation() {
+        UserModel user = new UserModel();
+        user.setNome("");                         // inválido
+        user.setEmail("valido@example.com");      // válido
+
+        Set<ConstraintViolation<UserModel>> violations = validator.validate(user);
+
+        assertEquals(1, violations.size());                       // espera 1 violação
+        ConstraintViolation<UserModel> v = violations.iterator().next();
+        assertEquals("name", v.getPropertyPath().toString());     // foi o campo name
+    }
 
 }
