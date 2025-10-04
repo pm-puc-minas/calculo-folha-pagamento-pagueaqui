0064069cdd15428fc56baf82fd20997ef93719f1
Milena Cardoso <mile.araujocar@gmail.com>
2025-10-04 18:11:37 -0300

    Injetando repo no service

--- hrpayroll/src/main/java/com/example/hrpayroll/service/FolhaPagamentoService.java
+++ hrpayroll/src/main/java/com/example/hrpayroll/service/FolhaPagamentoService.java
@@ -1,4 +1,14 @@
 package com.example.hrpayroll.service;
 
+import com.example.hrpayroll.repository.FolhaPagamentoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FolhaPagamentoService {
+    private final FolhaPagamentoRepository repository
+
+    public FolhaPagamentoService (FolhaPagamentoRepository repository) {
+        this.repository = repository;
+
+            }
 }
