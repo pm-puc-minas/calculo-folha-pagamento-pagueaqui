6a482f118b694f6a319dae76dacd342d364d2c3e
Milena Cardoso <mile.araujocar@gmail.com>
2025-10-04 17:55:02 -0300

    Implementando controller p/ FolhaPagamento

---
+++ hrpayroll/src/main/java/com/example/hrpayroll/controller/FolhaPagamentoController.java
@@ -0,0 +1,11 @@
+package com.example.hrpayroll.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/folhas")
+
+public class FolhaPagamentoController {
+
+}
