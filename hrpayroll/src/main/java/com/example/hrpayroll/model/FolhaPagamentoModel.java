984066164ecc9b06ea376a1e1409a09ab60ef71a
Milena Cardoso <mile.araujocar@gmail.com>
2025-10-04 17:42:34 -0300

    Implementando Model para FolhaPagamento

---
+++ hrpayroll/src/main/java/com/example/hrpayroll/model/FolhaPagamentoModel.java
@@ -0,0 +1,37 @@
+package com.example.hrpayroll.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class FolhaPagamentoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull (message = "A empresa é obrigatoria")
+    @ManyToOne(fetch = FetchType.LAZY)  // permite que muitas folhas de pagto pertençam a uma empresa
+    @JoinColumn(name = "company_id", nullable = false)
+    private CompanyModel company;
+
+    @NotNull (message = "A data da folha é obrigatoria")
+    private LocalDate dataFolha;
+
+    @NotNull (message = "O valor total é obrigatorio")
+    private BigDecimal valorTotal;
+
+    private Integer numeroHorasTrabalhadas;
+    private BigDecimal salarioBase;
+    private String observacoes;
+
+}
