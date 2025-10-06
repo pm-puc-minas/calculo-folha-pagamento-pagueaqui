1b36246753fde2d1d153fe874e0cbe1d78da29d9
MarinaSDIniz <marinasilvadiniz@gmail.com>
2025-10-05 23:10:11 -0300

    estrutura classe Descontos criada

---
+++ hrpayroll/src/main/java/com/example/hrpayroll/model/DescontosModel.java
@@ -0,0 +1,19 @@
+package com.example.hrpayroll.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DescontosModel {
+
+}
