764787a27c58eaa8f4e449df63e2efc740482b6e
caiofelixreis <caiofelix.reis@gmail.com>
2025-10-09 19:40:03 -0300

    feat: create adicional noturno e teste

--- hrpayroll/src/main/java/com/example/hrpayroll/service/AdicionalService.java
+++ hrpayroll/src/main/java/com/example/hrpayroll/service/AdicionalService.java
@@ -4,20 +4,27 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AdicionalService {
+    private static final Double TAXA_PERICULOSIDADE = 0.3;
+    private static final Double TAXA_HORA_EXTRA = 2.0;
 
-    public Double calcularAdicionalNoturno(Double salario) {
-        return salario;
+    public Double calcularAdicionalNoturno(Double salarioLiquido) {
+        return salarioLiquido;
     }
 
-    public Double calcularAdicionalInsalubridade(Double salario) {
-        return salario;
+    public Double calcularAdicionalInsalubridade(Double salarioLiquido) {
+        return salarioLiquido;
     }
 
-    public Double calcularAdicionalPericulosidade(Double salario) {
-        return salario;
+    public Double calcularAdicionalPericulosidade(Double salarioLiquido) {
+        Double adicional = salarioLiquido * TAXA_PERICULOSIDADE;
+        
+        return salarioLiquido + adicional;
     }
 
     public Double calcularAdicionalHorasExtras(Long horasExtras, Double salarioLiquido) {
-        return salarioLiquido;
+        Double valorHora = salarioLiquido / 220;
+        Double totalHorasExtra = valorHora * horasExtras * TAXA_HORA_EXTRA;
+
+        return salarioLiquido + totalHorasExtra;
     }
 }
