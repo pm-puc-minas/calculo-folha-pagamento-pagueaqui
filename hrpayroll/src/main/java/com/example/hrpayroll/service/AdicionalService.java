de4fe54bcdccaa9f3682924d1194afcdf280b66e
Milena Cardoso <mile.araujocar@gmail.com>
2025-10-09 19:34:27 -0300

    feat: add calculo de insalubridade

--- hrpayroll/src/main/java/com/example/hrpayroll/service/AdicionalService.java
+++ hrpayroll/src/main/java/com/example/hrpayroll/service/AdicionalService.java
@@ -4,15 +4,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AdicionalService {
+    private static final double SALARIO_MINIMO = 1.518;
 
+    // normalmente equivale a 20% do salario normal
     public Double calcularAdicionalNoturno(Double salario) {
         Double adicional = salario * 0.20;
         return salario + adicional;
     }
 
+    // levando em consideração 20% (grau medio)
     public Double calcularAdicionalInsalubridade(Double salario) {
-
-        return salario;
+        Double insalubridade = SALARIO_MINIMO * 0.20;
+        return salario + insalubridade;
     }
 
     public Double calcularAdicionalPericulosidade(Double salario) {
