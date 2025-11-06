a7ba31d37b2ef7167803fa5cbd2e54c51b4c7a9c
Oieusouopi <rafasc866@gmail.com>
2025-11-06 17:48:50 -0300

    proventos

---
+++ hrpayroll/src/main/java/com/example/hrpayroll/controller/ProventosController.java
@@ -0,0 +1,11 @@
+package com.example.hrpayroll.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/proventos")
+public class ProventosController {
+
+}
---
+++ hrpayroll/src/main/java/com/example/hrpayroll/service/ProventosService.java
@@ -0,0 +1,10 @@
+package com.example.hrpayroll.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProventosService {
+
+
+}
