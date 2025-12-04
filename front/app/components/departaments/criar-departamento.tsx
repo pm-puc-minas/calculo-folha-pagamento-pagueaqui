59a78d36947f74e0374dcfeda784544bfaa94e00
MarinaSDIniz <marinasilvadiniz@gmail.com>
2025-12-03 23:23:02 -0300

    adição de componentes

---
+++ front/app/components/departaments/criar-departamento.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+import { Aside } from '@/aside/Aside'
+import { Header } from '@/header/Header'
+
+export function departamentoModulo (){
+    return (
+        <>
+            <Header></Header>
+            <Aside></Aside>
+
+        </>
+    )
+}
